d5e7bbbfe9fdd540d866e3c4e7d26f8037d91185
Sarath Chandramohan <sarath90941@gmail.com>
2025-07-09 12:52:07 +0530

    Update technician.php

--- technician.php
+++ technician.php
@@ -81,9 +81,10 @@ $user = getCurrentUser();
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 <script>
+    let itemToProcess = null;
+
     document.addEventListener('DOMContentLoaded', function() {
         const codeReader = new ZXing.BrowserMultiFormatReader();
-        let itemToProcess = null; 
 
         loadTechnicianEquipment();
 
