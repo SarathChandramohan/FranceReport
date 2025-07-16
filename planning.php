f879bcc8a464cb9971ae02a7ca6b352415c1d539
Sarath Chandramohan <sarath90941@gmail.com>
2025-07-16 12:03:43 +0530

    Update planning.php

--- planning.php
+++ planning.php
@@ -99,7 +99,7 @@ $default_color = $predefined_colors[0];
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <form id="missionForm">
-                    <div class="modal-header bg-primary text-white">
+                    <div class="modal-header bg-light">
                         <h5 class="modal-title" id="missionFormModalLabel">Nouvelle Mission</h5>
                         <button type="button" class="close text-white" data-dismiss="modal">&times;</button>
                     </div>
