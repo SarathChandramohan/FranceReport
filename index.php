2b8282bd9510289caae0306dc5f7009b9c91768a
Sarath Chandramohan <sarath90941@gmail.com>
2025-08-23 12:11:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ function connectDB() {
             $connectionInfo = array(
                 "UID" => "francerecordloki",
                 "pwd" => "Hesoyam@2025",
-                "Database" => "Francerecord",
+                "Database" => "Francerecord_2",
                 "LoginTimeout" => 30,
                 "Encrypt" => 1,
                 "TrustServerCertificate" => 0
